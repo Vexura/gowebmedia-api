5e69c7a338adec56edcabc09c9b396a215c9c276
CookieMC337 <51511368+CookieMC337@users.noreply.github.com>
2023-07-05 01:23:56 +0200

    Update Manage.php
    fixed urls

--- src/RootServer/Manage/Manage.php
+++ src/RootServer/Manage/Manage.php
@@ -19,38 +19,38 @@ class Manage
     }
 
     public function get(int $vm_id){
-        return $this->GoWebMediaAPI->get('products/rootserver/' . $vm_id . '/get');
+        return $this->GoWebMediaAPI->get('products/rootserver/manage/' . $vm_id . '/get');
     }
 
     public function stats(int $vm_id){
-        return $this->GoWebMediaAPI->get('products/rootserver/' . $vm_id . '/stats');
+        return $this->GoWebMediaAPI->get('products/rootserver/manage/' . $vm_id . '/stats');
     }
 
     public function start(int $vm_id){
-        return $this->GoWebMediaAPI->post('products/rootserver/' . $vm_id . '/start');
+        return $this->GoWebMediaAPI->post('products/rootserver/manage/' . $vm_id . '/start');
     }
 
     public function kill(int $vm_id){
-        return $this->GoWebMediaAPI->post('products/rootserver/' . $vm_id . '/stop');
+        return $this->GoWebMediaAPI->post('products/rootserver/manage/' . $vm_id . '/stop');
     }
 
     public function stop(int $vm_id){
-        return $this->GoWebMediaAPI->post('products/rootserver/' . $vm_id . '/shutdown');
+        return $this->GoWebMediaAPI->post('products/rootserver/manage/' . $vm_id . '/shutdown');
     }
 
     public function restart(int $vm_id){
-        return $this->GoWebMediaAPI->post('products/rootserver/' . $vm_id . '/restart');
+        return $this->GoWebMediaAPI->post('products/rootserver/manage/' . $vm_id . '/restart');
     }
 
     public function reinstall(int $vm_id, int $os_id, string $root_password){
-        return $this->GoWebMediaAPI->post('products/rootserver/' . $vm_id . '/reinstall', [
+        return $this->GoWebMediaAPI->post('products/rootserver/manage/' . $vm_id . '/reinstall', [
             "password" => $root_password,
             "os" => $os_id
         ]);
     }
 
     public function delete(int $vm_id){
-        return $this->GoWebMediaAPI->delete('products/rootserver/' . $vm_id . '/delete');
+        return $this->GoWebMediaAPI->delete('products/rootserver/manage/' . $vm_id . '/delete');
     }
 
     public function create(string $cpu_type, string $hostname, int $cores, int $disk, int $ram, int $backups, string $root_password, int $os_id, int $ipv4, int $ipv6 = 0){
